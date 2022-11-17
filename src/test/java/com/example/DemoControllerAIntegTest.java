78ef69c93a6af05404c7c3fcf144b94c8c815d9b
Marcos Canales <marcos.canales@rakuten.com>
2022-11-17 18:39:34 +0900

    Remove unused models

--- src/test/java/com/example/DemoControllerAIntegTest.java
+++ src/test/java/com/example/DemoControllerAIntegTest.java
@@ -1,24 +1,19 @@
 package com.example;
 
 import com.example.model.MultiLocaleValueDTO;
-import com.example.model.ReqDTO;
-import com.example.model.ResDTO;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
 class DemoControllerAIntegTest {
